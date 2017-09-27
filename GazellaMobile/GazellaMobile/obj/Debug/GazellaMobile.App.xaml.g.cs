f570ca87e701b4d79f23b0f4f1015b0f4e2e54a0
enmanuelcruz <enmanuelcruzdejesus@gmail.com>
2017-09-27 16:32:42 -0400

    App Theaming and Styles

--- GazellaMobile/GazellaMobile/obj/Debug/GazellaMobile.App.xaml.g.cs
+++ GazellaMobile/GazellaMobile/obj/Debug/GazellaMobile.App.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace GazellaMobile {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\GazellaMobile\\GazellaMobile\\GazellaMobile\\App.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\ecruz\\Source\\Repos\\GazellaMobile\\GazellaMobile\\GazellaMobile\\App.xaml")]
     public partial class App : global::Xamarin.Forms.Application {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
