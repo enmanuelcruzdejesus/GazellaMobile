d6d9f9a204a48493085d2260181d573c575825f0
enmanuelcruz <enmanuelcruzdejesus@gmail.com>
2017-09-27 15:45:19 -0400

    Network Availability class implemented

---
+++ GazellaMobile/GazellaMobile/Helpers/GDSNetworkConnectivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+
+namespace GazellaMobile.Helpers
+{
+    public class GDSNetworkConnectivity
+    {
+        public static bool isConneted()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+                return true;
+
+            return false;
+        }
+    }
+}
